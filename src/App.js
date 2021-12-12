e4cd9a8723457c939fedc1f6eeedf3cb002265b9
theowiersema <theowiersema@users.noreply.github.com>
2021-12-11 21:31:23 -0600

    Added ability to switch data source

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {useEffect, useState} from 'react';
 import hamlet from './hamlet';
 import job from './job';
+import jabberwocky from './jabberwocky';
 import {initialize,buildSentence} from './logic';
 
 const App = () => {
 
   const [markovModel,setMarkovModel] = useState(null);
-  const [input,setInput] = useState(job);
+  const [input,setInput] = useState(hamlet);
+  const [inputSource, setInputSource] = useState('hamlet');
   const [output,setOutput] = useState('');
   const [buttonText,setButtonText] = useState('Generate');
 
@@ -14,15 +16,32 @@ const App = () => {
     setMarkovModel(initialize(input)); // initialize the markov model 
   },[input])
 
+  const switchInputSource = (value) => {
+    setInputSource(value);
+    switch(value) {
+      case 'hamlet': setInput(hamlet); break;
+      case 'job': setInput(job); break;
+      case 'jabberwocky': setInput(jabberwocky); break;
+      default: throw new Error();
+    }
+  }
+
   const generate = () => {
-    
-    setOutput(buildSentence(markovModel,100,input)); // build sentence
+    const maxNumberOfWords=100;
+    setOutput(buildSentence(markovModel,maxNumberOfWords,input));
     setButtonText('Regenerate');
   }
 
   return (
     <div className="content">
       <div className="left">
+       <p>Choose from several pre-selected sources:</p>
+        <select value={inputSource} onChange={event => switchInputSource(event.target.value)}>
+          <option value="hamlet">Hamlet</option>
+          <option value="job">The Book of Job</option>
+          <option value="jabberwocky">Jabberwocky</option>
+        </select>
+        <p>Or enter in your own text:</p>
         <textarea value={input} onChange={event => setInput(event.target.value)}></textarea>
         <button onClick={generate}>{buttonText} &rarr;</button>
       </div>
